f58098bdeb13a274f4df73d34962e93d2cd79cf0
Konstantin Karatanas <k.karatanas@mail.ru>
2018-06-29 23:17:05 +0300

    Uploading

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning!" : "Goood Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+                return View("Thanks", guest);
+            else
+                return View();
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "PLease, enter your Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "PLease, enter your Surname")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "PLease, enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Incorrect email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "PLease, enter your phone")]
+        [RegularExpression(@"[\d]{10}", ErrorMessage = "Incorrect number (08********)")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please , notice if you will take part in the party")]
+        public bool? WillAttend { get; set; }
+    }
+}
